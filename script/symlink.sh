8cb9af335c376f601b9a8d2eec6bb610c8a05d1f
Olivier Lauzon <olauzon@gmail.com>
2010-05-14 16:53:24 -0400

    Don't automatically symlink gvimrc
    Would be nice to find out how to do platform specific configurations.

--- script/symlink.sh
+++ script/symlink.sh
@@ -1,8 +1,8 @@
 rm -rf ~/.vimrc
 ln -s ~/dotvim/current/vimrc ~/.vimrc
 
-rm -rf ~/.gvimrc
-ln -s ~/dotvim/current/gvimrc ~/.gvimrc
+#rm -rf ~/.gvimrc
+#ln -s ~/dotvim/current/gvimrc ~/.gvimrc
 
 rm -rf ~/.vim
 ln -s ~/dotvim/current/vim ~/.vim
